50604237cc5ac45fc36013b584b77bb29942a958
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-11-06 18:06:00 +0900

    Create MediaRecorderSample.js

---
+++ docs/MediaRecorderSample.js
@@ -0,0 +1,51 @@
+var stream = null;
+var recordChunks = null;
+var recorder = null;
+var codecType = null;
+
+navigator.mediaDevices.getUserMedia({video: true, audio:false}).then(gumStream => {
+    stream = gumStream;
+    document.querySelectorAll('recbtn').forEach(btn => btn.disabled = false);
+});
+
+
+function handleDataAvailable(event) {
+    recordChunks.push(event.data);
+}
+
+function rec(recType) {
+    codecType = recType;
+    recordChunks = [];
+    recorder = new MediaRecorder(stream, {mimeType: `video/webm; codecs=${codecType}`});
+    document.querySelectorAll('recbtn').forEach(btn => btn.disabled = true);
+    recStop.disabled = false;
+    recorder.ondataavailable = handleDataAvailable;
+}
+
+recVP8.onclick = function() {
+    rec('vp8');
+}
+recVP9.onclick = function() {
+    rec('vp9');
+}
+recH264.onclick = function() {
+    rec('h264');
+}
+
+recStop.onclick = function() {
+    document.querySelectorAll('recbtn').forEach(btn => btn.disabled = false);
+    recorder.stop();
+    recorder = null;
+    recStop.disabled = true;
+}
+
+function download() {
+  var blob = new Blob(recordChunks, {type: 'video/webm'});
+  var url = URL.createObjectURL(blob);
+  var a = document.createElement('a');
+  //a.style.display = 'none';
+  a.href = url;
+  a.download = codecType + '.webm';
+  //document.body.appendChild(a);
+  a.click();
+}
