7da5bd537a077dda3c25fcf87aa681a909d8bfcc
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-11-06 18:36:34 +0900

    Update MediaRecorderSample.js

--- docs/MediaRecorderSample.js
+++ docs/MediaRecorderSample.js
@@ -58,14 +58,14 @@ recStop.onclick = function() {
     if(recordChunks && recordChunks.length) dl.disabled = false;
 }
 
-function download() {
+dl.onclick = function () {
   var blob = new Blob(recordChunks, {type: 'video/webm'});
   var url = URL.createObjectURL(blob);
     
   var a = document.createElement('a');
-  a.style.display = 'none';
+  //a.style.display = 'none';
   a.href = url;
   a.download = codecType + '.webm';
-  document.body.appendChild(a);
+  //document.body.appendChild(a);
   a.click();
 }
